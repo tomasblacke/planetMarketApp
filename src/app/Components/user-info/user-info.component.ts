620c5f65213d0fa37b9a913e5d61c7371121dbd3
Nico Flores <nicofloresp2004@gmail.com>
2024-11-02 13:52:41 -0300

    correccion de error en la muestra de datos del user

--- src/app/Components/user-info/user-info.component.ts
+++ src/app/Components/user-info/user-info.component.ts
@@ -3,6 +3,12 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../../Services/user-auth.service';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
+interface UserData {
+  name: string;
+  email: string;
+  lastname: string;
+}
+
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
@@ -11,7 +17,7 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 export class UserInfoComponent implements OnInit {
   userForm: FormGroup = new FormGroup({
     name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     lastname: new FormControl('', Validators.required)
   });
 
@@ -29,15 +35,21 @@ export class UserInfoComponent implements OnInit {
           .get()
           .subscribe(doc => {
             if (doc.exists) {
-              const userData = doc.data();
+              const userData = doc.data() as UserData;  // Tipado de userData
               console.log('Setting form data:', userData);
               this.userForm.setValue({
-                name: userData?.name || '',
-                email: userData?.email || '',
-                lastname: userData?.lastname || ''
+                name: userData.name || '',
+                email: userData.email || '',
+                lastname: userData.lastname || ''
               });
+            } else {
+              console.log("No se encontraron datos para el usuario.");
             }
+          }, error => {
+            console.error("Error al obtener los datos del usuario:", error);
           });
+      } else {
+        console.log("No hay un usuario logueado actualmente.");
       }
     });
   }
@@ -50,7 +62,13 @@ export class UserInfoComponent implements OnInit {
             name: this.userForm.get('name')?.value,
             lastname: this.userForm.get('lastname')?.value
           };
-          this.firestore.collection('users').doc(user.uid).update(updateData);
+          this.firestore.collection('users').doc(user.uid).update(updateData)
+            .then(() => {
+              console.log('Datos de usuario actualizados exitosamente.');
+            })
+            .catch(error => {
+              console.error('Error al actualizar los datos de usuario:', error);
+            });
         }
       });
     }
