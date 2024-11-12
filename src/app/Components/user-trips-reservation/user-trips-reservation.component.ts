7c0e021b49709ad12954ac21e7f1bc89246fabe2
tomasblacke <tomaseblack@outlook.com>
2024-11-11 23:03:49 -0300

    bosquejo de pasaje

--- src/app/Components/user-trips-reservation/user-trips-reservation.component.ts
+++ src/app/Components/user-trips-reservation/user-trips-reservation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Observable } from 'rxjs';
 import { UserReservationsService } from 'src/app/Services/user-reservations.service';
+import { PdfService } from 'src/app/Services/pdf.service';
 
 
 @Component({
@@ -13,13 +14,19 @@ export class UserTripsReservationComponent implements OnInit {
 
   userReservations$!: Observable<any[]>;
 
-  constructor(private userService: UserReservationsService) { }
+  constructor(private userService: UserReservationsService, private pdfService : PdfService) { }
 
   ngOnInit(): void {
     // Llamamos al servicio para obtener las reservas del usuario
     this.userReservations$ = this.userService.getUserReservations();
   }
-
- 
- 
+  
+  async downloadTicketPDF(reservation: any): Promise<void> {
+    try {
+      await this.pdfService.generateReservationPDF(reservation);
+    } catch (error) {
+      console.error('Error downloading PDF:', error);
+      // Aquí puedes manejar el error como prefieras (mostrar un mensaje al usuario, etc.)
+    }
+  }
 }
