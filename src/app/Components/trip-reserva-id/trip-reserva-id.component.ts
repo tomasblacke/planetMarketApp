92d8a516130bc10e9a8ff1187aecdd20b3653221
Pedro Gomez <augustopgomez1994@gmail.com>
2024-11-13 13:00:48 -0300

    trip dinamicos-estaticos

--- src/app/Components/trip-reserva-id/trip-reserva-id.component.ts
+++ src/app/Components/trip-reserva-id/trip-reserva-id.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { SpaceTrip } from 'src/app/Services/travel-reservations.service';
 import { TravelReservationsService } from 'src/app/Services/travel-reservations.service';
+import { CommentListComponent } from 'src/app/Components/comment-list/comment-list.component';
 
 interface Passenger {
   name: string;
@@ -14,34 +16,32 @@ interface Passenger {
   styleUrls: ['./trip-reserva-id.component.css']
 })
 export class TripReservaIdComponent implements OnInit {
-  trip: SpaceTrip | undefined;
+  trip$!: Observable<SpaceTrip | undefined>;
   mainPassenger: Passenger = { name: '', email: '' };
   companions: Passenger[] = [];
   totalPrice: number = 0; 
   successMessage: string = ''; // Variable para el mensaje de éxito
+  isImageLoading: boolean = true; // Flag para mostrar cargador de imagen
+
+  // Declarar ViewChild para acceder a commentsListComponent
+  @ViewChild('commentsListComponent') commentsListComponent!: CommentListComponent;
 
   constructor(
     private route: ActivatedRoute,
-    private travelReservationsService: TravelReservationsService
+    private travelReservationsService: TravelReservationsService,
+    private cd: ChangeDetectorRef // Inyectar ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
     const tripId = this.route.snapshot.paramMap.get('id')!;
-    this.travelReservationsService.getTripById(parseInt(tripId, 10)).subscribe(
-      trip => {
-        this.trip = trip;
-        if (this.trip) {
-          this.calculateTotalPrice();
-        } else {
-          console.error(`Trip with ID ${tripId} not found.`);
-        }
-      },
-      error => {
-        console.error(`Error loading trip with ID ${tripId}:`, error);
-      }
-    );
+    this.trip$ = this.travelReservationsService.getTripById(parseInt(tripId, 10));
   }
-  
+
+  // Función llamada cuando la imagen se carga
+  onImageLoad() {
+    this.isImageLoading = false; // La imagen se ha cargado
+  }
+
   addCompanion() {
     this.companions.push({ name: '', email: '' });
     this.calculateTotalPrice();
@@ -53,40 +53,44 @@ export class TripReservaIdComponent implements OnInit {
   }
 
   calculateTotalPrice() {
-    if (this.trip) {
-      const numberOfPassengers = this.companions.length + 1; // +1 for main passenger
-      this.totalPrice = this.trip!.priceByPassanger * numberOfPassengers;
-    }
+    const numberOfPassengers = this.companions.length + 1; // +1 for main passenger
+    this.trip$?.subscribe(trip => {
+      if (trip) {
+        this.totalPrice = trip.priceByPassanger * numberOfPassengers;
+      }
+    });
   }
 
   onSubmit() {
-    if (this.trip) {
-      const totalPassengers = this.companions.length + 1;
-  
-      this.travelReservationsService.processPurchase(this.trip.id.toString(), totalPassengers)
-        .then(response => {
-          if (response.success) {
-            console.log('Reservation submitted successfully:', response.transaction);
+    this.trip$?.subscribe(trip => {
+      if (trip) {
+        const totalPassengers = this.companions.length + 1;
+    
+        this.travelReservationsService.processPurchase(trip.id.toString(), totalPassengers)
+          .then(response => {
+            if (response.success) {
+              console.log('Reservation submitted successfully:', response.transaction);
 
-            // Limpiar el formulario
-            this.mainPassenger = { name: '', email: '' };
-            this.companions = [];
-            this.calculateTotalPrice();
+              // Limpiar el formulario
+              this.mainPassenger = { name: '', email: '' };
+              this.companions = [];
+              this.calculateTotalPrice();
 
-            // Mostrar mensaje de éxito
-            this.successMessage = 'Reserva exitosa';
+              // Mostrar mensaje de éxito
+              this.successMessage = 'Reserva exitosa';
 
-            // Ocultar mensaje después de unos segundos
-            setTimeout(() => {
-              this.successMessage = '';
-            }, 3000);
-          } else {
-            console.error('Error processing reservation:', response.message);
-          }
-        })
-        .catch(error => {
-          console.error('Error during reservation process:', error);
-        });
-    }
+              // Ocultar mensaje después de unos segundos
+              setTimeout(() => {
+                this.successMessage = '';
+              }, 3000);
+            } else {
+              console.error('Error processing reservation:', response.message);
+            }
+          })
+          .catch(error => {
+            console.error('Error during reservation process:', error);
+          });
+      }
+    });
   }
 }
