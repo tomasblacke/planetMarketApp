939f7e9422731c9cb6a9195b65c942a4adab358c
Pedro Gomez <augustopgomez1994@gmail.com>
2024-11-20 23:14:55 -0300

    planet-card .ts
    metodos para scrolling

--- src/app/Components/planet-cards/planet-cards.component.ts
+++ src/app/Components/planet-cards/planet-cards.component.ts
@@ -1,29 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { PlanetService, Planet } from '../../Services/planet.service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-planet-cards',
   templateUrl: './planet-cards.component.html',
-  styleUrl: './planet-cards.component.css'
+  styleUrls: ['./planet-cards.component.css']
 })
 export class PlanetCardsComponent implements OnInit {
   planets: Planet[] = [];
-  constructor(private planetService: PlanetService, private router: Router) { }
+
+  @ViewChild('planetGrid', { static: true }) planetGrid!: ElementRef;
+
+  constructor(private planetService: PlanetService, private router: Router) {}
 
   ngOnInit() {
-    this.planetService.getPlanets().subscribe(
-      planets => {
-        this.planets = planets;
-      }
-    );
+    this.planetService.getPlanets().subscribe(planets => {
+      this.planets = planets;
+    });
   }
+
   viewPlanetDetails(planetId: number) {
     console.log('Navigating to planet details:', planetId);
-    
     this.router.navigate(['/planets', planetId]);
   }
-  
 
+  scrollLeft(): void {
+    const container = this.planetGrid.nativeElement;
+    container.scrollBy({ left: -300, behavior: 'smooth' }); // Desplaza 300px a la izquierda
+  }
 
+  scrollRight(): void {
+    const container = this.planetGrid.nativeElement;
+    container.scrollBy({ left: 300, behavior: 'smooth' }); // Desplaza 300px a la derecha
+  }
 }
