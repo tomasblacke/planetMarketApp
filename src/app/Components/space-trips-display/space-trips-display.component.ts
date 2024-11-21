1a8b7a34f936c7eec97667b8cac0aa6d22bc226f
Pedro Gomez <augustopgomez1994@gmail.com>
2024-11-21 00:12:11 -0300

    timestamp - date

--- src/app/Components/space-trips-display/space-trips-display.component.ts
+++ src/app/Components/space-trips-display/space-trips-display.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 import { TravelReservationsService, SpaceTrip } from '../../Services/travel-reservations.service';
-
+import { FirebaseTimestamp } from '../../Services/travel-reservations.service';
 @Component({
   selector: 'app-space-trips-display',
   templateUrl: './space-trips-display.component.html',
@@ -38,5 +38,20 @@ export class SpaceTripsDisplayComponent implements OnInit {
   // Método para redirigir al detalle de un viaje
   reserveTrip(tripId: number) {
     this.router.navigate(['/trips', tripId]); 
+  }
+
+  convertToDate(date: Date | FirebaseTimestamp | null): Date | null {
+    if (date && typeof date === 'object' && 'seconds' in date) {
+      return new Date(date.seconds * 1000);
+    }
+    return date instanceof Date ? date : null;
+  }
+  
+  getFormattedDate(date: Date | FirebaseTimestamp | null): string {
+    const validDate = this.convertToDate(date);
+    if (!validDate || isNaN(validDate.getTime())) {
+      return 'Not specified';
+    }
+    return validDate.toLocaleDateString('en-GB');
   }
 }
