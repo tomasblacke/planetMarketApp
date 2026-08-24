c0083d065c357421710ffcf5f742d08cc7e54ef3
Tomas Black <tomaseblack@outlook.com>
2026-08-23 22:29:07 -0300

    Bloqueo de clicke del formulario para trip. Modificacion de panel, en los viajes ahora despliega la informacion de los pasajeros

--- src/app/Components/user-trips-reservation/user-trips-reservation.component.ts
+++ src/app/Components/user-trips-reservation/user-trips-reservation.component.ts
@@ -10,11 +10,29 @@ import { UserReservationsService } from 'src/app/Services/user-reservations.serv
 export class UserTripsReservationComponent implements OnInit {
 
   userReservations$!: Observable<any[]>;
+  reservaAbierta: string | null = null;
 
   constructor(private userService: UserReservationsService) { }
 
   ngOnInit(): void {
     // Llamamos al servicio para obtener las reservas del usuario
     this.userReservations$ = this.userService.getUserReservations();
+  }
+
+  // Abre la reserva clickeada, y si ya estaba abierta la cierra
+  toggleReserva(tripId: string) {
+    this.reservaAbierta = this.reservaAbierta === tripId ? null : tripId;
+  }
+
+  // Fomartea timestamp de Firebase
+  getFormattedDate(date: any): string {
+    if (!date) {
+      return 'Not specified';
+    }
+    const validDate = date.seconds ? new Date(date.seconds * 1000) : new Date(date);
+    if (isNaN(validDate.getTime())) {
+      return 'Not specified';
+    }
+    return validDate.toLocaleDateString('en-GB');
   }
 }
